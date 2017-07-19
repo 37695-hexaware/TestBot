91a57e780e5b0f216ba91fa794a5ec1d7c307909
37695-hexaware <37695.hexaware@gmail.com>
2017-07-19 10:44:40 +0530

    Update conversationServiceNew.js

--- src/com/service/conversationServiceNew.js
+++ src/com/service/conversationServiceNew.js
@@ -96,14 +96,6 @@ rl.on('error',function(err){
 
 
 function checkResponse(responseFromApi,expectedResponse ){
-
-/* for (var i = 0, L=responseFromApi.length ; i < L; i++) {
-   console.log("length is "+L);
-  responseFromApi[i]=responseFromApi[i].toLowerCase();
-}
-  for (var i = 0, L=expectedResponse.length ; i < L; i++) {
-  expectedResponse[i]=expectedResponse[i].toLowerCase();
-}  */
    console.log("API::"+responseFromApi+"EXPECTED::"+expectedResponse);
 if(expectedResponse.indexOf(responseFromApi) > -1) {
   console.log("test case passed");
